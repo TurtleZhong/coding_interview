e034cc78d47552a0df2c97c2d01facb6ace2447d
TurtleZhong <xinliangzhong@foxmail.com>
2018-04-09 17:21:02 +0800

    question 05 finished

--- src/05_replace_spaces.cpp
+++ src/05_replace_spaces.cpp
@@ -1,13 +1,56 @@
-//
-// Created by m on 4/9/18.
-//
+/**
+ * 将空格替换成%20
+ */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+void replaceSpaces(char *str, const int &length)
+{
+    if(str == nullptr || length <= 0)
+        return;
+    // Calc the num of spaces;
+    int num_space = 0;
+    for(int i = 0; i < length; ++i)
+    {
+        if(str[i] == ' ')
+            num_space++;
+    }
+    int j = length + 2 * num_space -1;
+    char* str_copy = str;
+
+    for(int i = length -1; i >= 0; i--)
+    {
+        cout << "i = " << i << endl;
+        if(str_copy[i] == ' ')
+        {
+            str[j--] = '0';
+            str[j--] = '2';
+            str[j--] = '%';
+        } else
+        {
+            str[j--] = str_copy[i];
+        }
+
+
+    }
+
+    cout << "num_space = " << num_space << endl;
+}
+
 int main()
 {
-    cout << "Hello" << endl;
+    char str[100] = " We are Happy .";
+
+    cout << "str.length = " << strlen(str) << endl;
+    cout << "Before replace:\n" << str << endl;
+    replaceSpaces(str, strlen(str));
+    cout << "After replace:\n" << str << endl;
+
+    int x = 1, y = 5;
+    cout << (x ^ y) << endl;
+
     return 0;
 }
